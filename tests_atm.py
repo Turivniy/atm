9f794b493a9f7786e95b22f246fcd350d46c215a
Alexander <32265866+Alexxfromgit@users.noreply.github.com>
2017-12-19 03:04:13 +0200

    Update tests_atm.py

--- tests_atm.py
+++ tests_atm.py
@@ -3,6 +3,7 @@ import unittest
 from atm import Atm
 from atm_exceptions import *
 
+
 class TestAtm(unittest.TestCase):
 
     def setUp(self):
@@ -13,6 +14,14 @@ class TestAtm(unittest.TestCase):
         money = self.atm.get_money(1000)
         self.assertEqual(1000, money)
 
+    def test_atm_rise_money(self):
+        rise = self.atm._rise_money(1000)
+        self.assertEqual(11000, rise)
+
+    def test_atm_check_balance(self):
+        check = self.atm.check_balance()
+        self.assertEqual(10000, check)
+
 
 class TestAtmExceptions(unittest.TestCase):
 
@@ -20,11 +29,31 @@ class TestAtmExceptions(unittest.TestCase):
         self.atm = Atm()
         self.atm.enter_pin(777)
 
-    def test_exception(self):
+    def test_exception_atm_balance(self):
         try:
             self.atm.get_money(1000000)
         except Exception as e:
-            self.assertEqual('Atm balance is no enough!!!', e.message)
+            self.assertEqual("Atm balance is no enough!!!", e.message)
+
+    def test_exception_incorrect_pin(self):
+        try:
+            self.atm.enter_pin(111)
+        except Exception as e:
+            self.assertEqual("Incorrect Pin!!!", e.message)
+
+    def test_exception_enter_pin_first(self):
+        try:
+            self.atm2 = Atm()
+            self.atm2.check_balance()
+        except Exception as e:
+            self.assertEqual("Enter pin first!!!", e.message)
+
+    def test_exception_attempts_over(self):
+        try:
+            self.atm2 = Atm()
+            self.atm2.__attempts = 0
+        except Exception as e:
+            self.assertEqual("Attempts are over!!!", e.message)
 
 
 if __name__ == '__main__':
